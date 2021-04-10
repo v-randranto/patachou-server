a36b5812edf5e71e5d921fb73b03203ee4657e7f
v-randranto <verorand@yahoo.com>
2021-04-10 22:06:59 +0200

    Corrections saisie photo

--- src/models/Account.js
+++ src/models/Account.js
@@ -1,17 +1,15 @@
 'use strict';
 
 const mongoose = require('mongoose');
-const bcrypt = require('bcryptjs')
-const crypto = require('crypto')
+const bcrypt = require('bcryptjs');
+const crypto = require('crypto');
 const jwt = require('jsonwebtoken');
 
 const cloudinary = require('cloudinary').v2;
 const sendEmail = require('../utils/sendEmail');
 const storePhoto = require('../utils/storePhoto');
 
-const {
-  getRandomInt
-} = require('../utils/randomNumber')
+const { getRandomInt } = require('../utils/randomNumber');
 
 const AccountSchema = new mongoose.Schema({
   pseudo: {
@@ -27,74 +25,89 @@ const AccountSchema = new mongoose.Schema({
 
   password: {
     type: String,
-    required: [true, "Le mot de passe est obligatoire"],
+    required: [true, 'Le mot de passe est obligatoire'],
     select: false,
   },
   resetPasswordToken: String,
   resetPasswordExpire: Date,
   presentation: {
     type: String,
-    default: 'Je prépare un pitch de présentation aux petits oignons...'
+    default: 'Je prépare un pitch de présentation aux petits oignons...',
   },
   photoUrl: {
-    type: String
+    type: String,
   },
 
   creationDate: {
     type: Date,
-    default: Date.now
+    default: Date.now,
   },
   modificationDate: {
     type: Date,
-    default: Date.now
-  }
+    default: Date.now,
+  },
 });
 
-AccountSchema.pre("save", async function (next) {
+AccountSchema.pre('save', async function (next) {
   if (!this.isModified) {
-    next()
+    next();
   }
 
-  if (this.isModified("password")) {
-    const salt = await bcrypt.genSalt(10)
-    this.password = await bcrypt.hash(this.password, salt)
+  if (this.isModified('password')) {
+    const salt = await bcrypt.genSalt(10);
+    this.password = await bcrypt.hash(this.password, salt);
   }
 
-  if (this.isModified("photoUrl")) {
-    this.photoUrl = await storePhoto(this.photoUrl)
-  } else {
-    this.photoUrl = process.env[`DEFAULT_AVATAR_${getRandomInt(4)}`]
+  if (this.isModified('photoUrl')) {
+    if (this.photoUrl) {
+      this.photoUrl = await storePhoto(this.photoUrl);
+    } else {
+      this.photoUrl =
+        process.env[
+          `DEFAULT_AVATAR_${getRandomInt(4)}`
+        ];
+    }
   }
-  next()
-})
+  next();
+});
 
 AccountSchema.methods.checkPassword = async function (password) {
-  return await bcrypt.compare(password, this.password)
-}
+  return await bcrypt.compare(password, this.password);
+};
 
 AccountSchema.methods.getSignedToken = function () {
-  return jwt.sign({
-    id: this._id,
-    pseudo: this.pseudo
-  }, process.env.TOKEN_KEY, {
-    expiresIn: process.env.TOKEN_EXPIRE
-  })
-}
+  return jwt.sign(
+    {
+      id: this._id,
+      pseudo: this.pseudo,
+    },
+    process.env.TOKEN_KEY,
+    {
+      expiresIn: process.env.TOKEN_EXPIRE,
+    }
+  );
+};
 
 AccountSchema.methods.getResetPasswordToken = function () {
-  const resetToken = crypto.randomBytes(20).toString("hex")
-  this.resetPasswordToken = crypto.createHash("sha256").update(resetToken).digest("hex")
-  this.resetPasswordExpire = Date.now() + (60 * 60 * 1000)
+  const resetToken = crypto.randomBytes(20).toString('hex');
+  this.resetPasswordToken = crypto
+    .createHash('sha256')
+    .update(resetToken)
+    .digest('hex');
+  this.resetPasswordExpire = Date.now() + 60 * 60 * 1000;
   return resetToken;
-}
+};
 
 AccountSchema.methods.sendRegisterEmail = async function (options, variables) {
-  return await sendEmail(options, "register", variables)
-}
+  return await sendEmail(options, 'register', variables);
+};
 
-AccountSchema.methods.sendResetPasswordEmail = async function (options, variables) {
-  return await sendEmail(options, "lostPassword", variables)
-}
+AccountSchema.methods.sendResetPasswordEmail = async function (
+  options,
+  variables
+) {
+  return await sendEmail(options, 'lostPassword', variables);
+};
 
 const Account = mongoose.model('Account', AccountSchema);
 module.exports = Account;
